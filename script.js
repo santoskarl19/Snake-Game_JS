4d6478a43bd5069430a3e051ed0d18c1b3bb5ff9
Karl Santos <santoskarl19@gmail.com>
2023-12-30 08:58:04 -0700

    implement: game food functions and elements

--- script.js
+++ script.js
@@ -2,20 +2,23 @@
 const board = document.getElementById('game-board');
 
 // define game variables
-let snake = [{x: 10, y: 10}];
-
+const gridSize = 20;
+let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
 
 // draw game map, snake, food
 function draw() {
     board.innerHTML = '';
     drawSnake();
+    drawFood();
 }
 
 // draw snake
 function drawSnake() {
-    snake.array.forEach(segment => {
+    snake.forEach(segment => {
         const snakeElement = createGameElement('div', 'snake');
         setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
     });
 }
 
@@ -29,5 +32,22 @@ function createGameElement(tag, className) {
 // Set the position of the snake or food
 function setPosition(element, position) {
     element.style.gridColumn = position.x;
-    
+    element.style.gridRow = position.y;
+}
+
+// Testing draw function
+draw();
+
+// Draw food function
+function drawFood() {
+    const foodElement = createGameElement('div', 'food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+}
+
+// Generate food function
+function generateFood() {
+    const x = Math.floor((Math.random() * gridSize) + 1);
+    const y = Math.floor((Math.random() * gridSize) + 1);
+    return { x, y };
 }
